adf248413eda05ea35079792980c4953d0ab562a
Héctor Gutiérrez <hectorgut47@gmail.com>
2018-01-03 21:53:33 +0100

    Changed execution type from application to server app

--- src/main/java/com/hector/distribuidos/trabajo/AmazonPriceList.java
+++ src/main/java/com/hector/distribuidos/trabajo/AmazonPriceList.java
@@ -1,5 +1,10 @@
 package com.hector.distribuidos.trabajo;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,19 +15,36 @@ public class AmazonPriceList {
 
 	public static void main(String[] args) {
 		
-		List<String> lista = new ArrayList<>();
-		lista.add("B00WKB2MQW");
-		lista.add("B0098DXQ5Q");
-		PriceList originalList = AmazonScraperUtils.unmarshalProducts();
-		List<String> filteredList = AmazonScraperUtils.mustCheckPrice(originalList, lista);
-		if (filteredList.size() > 0) {
-			AmazonScraper scraper = new AmazonScraper(filteredList);
-			PriceList list = scraper.getPrices();
-			AmazonScraperUtils.marshalProducts(list);
+		Socket client;
+		
+		try (ServerSocket ss = new ServerSocket(12000)) {
+			while (true) {
+				client = ss.accept();
+				BufferedReader reader = new BufferedReader(new InputStreamReader(client.getInputStream()));
+				String idProd;
+				List<String> idList = new ArrayList<>();
+				while ((idProd = reader.readLine()) != null) {
+					idList.add(idProd);
+				}
+				PriceList originalList = AmazonScraperUtils.unmarshalProducts();
+				List<String> filteredList = AmazonScraperUtils.mustCheckPrice(originalList, idList);
+				if (filteredList.size() > 0) {
+					AmazonScraper scraper = new AmazonScraper(filteredList);
+					PriceList list = scraper.getPrices();
+					AmazonScraperUtils.marshalProducts(list);
+				}
+				if (client != null) {
+					try {
+						client.close();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-		PriceList list2 = AmazonScraperUtils.unmarshalProducts();
-		list2.getPrices().forEach(p -> System.out.println(p.getId_prod() + " " + p.getPrice() + " " + p.getDate()));
-	
+		
 	}
 
 }
